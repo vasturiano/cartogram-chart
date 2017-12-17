6f19500863173a8599ca1c9d5b7698e6ddcb72c0
vasco <vastur@gmail.com>
2017-12-17 01:37:48 -0800

    Add bundler

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
+import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            format: 'umd',
+            name: 'Cartogram',
+            file: `dist/${name}.js`,
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        postCss({
+            plugins: [
+                postCssSimpleVars(),
+                postCssNested()
+            ]
+        }),
+        resolve(),
+        commonJs(),
+        babel({ exclude: 'node_modules/**' })
+    ],
+    banner: `// Version ${version} ${name} - ${homepage}`
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default } from './cartogram';
+import './cartogram.css';
